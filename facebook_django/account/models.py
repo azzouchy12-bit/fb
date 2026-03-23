42b076efbc1c642823f080cf2f7e5563913a1f91
azzouchy12-bit <azzouchy12@gmail.com>
2026-03-23 02:03:59 +0000

    Fix custom user manager superuser creation

--- facebook_django/account/models.py
+++ facebook_django/account/models.py
@@ -14,6 +14,8 @@ class CustomUserManager(UserManager):
     def _create_user(self, name, email, password, **extra_fields):
         if not email:
             raise ValueError("You have not provided a valid e-mail address")
+        if name is None:
+            name = ""
         email = self.normalize_email(email)
         user = self.model(email=email, name=name, **extra_fields)
         user.set_password(password)
@@ -21,11 +23,15 @@ class CustomUserManager(UserManager):
         return user
 
     def create_user(self, name=None, email=None, password=None, **extra_fields):
+        if name is None:
+            name = ""
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(name, email, password, **extra_fields)
 
     def create_superuser(self, name=None, email=None, password=None, **extra_fields):
+        if name is None:
+            name = "Admin"
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         return self._create_user(name, email, password, **extra_fields)
